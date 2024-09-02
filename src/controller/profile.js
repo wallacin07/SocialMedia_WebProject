5f3f8fcf93128920e1088e749354c55b5e9fb0c6
WallyDev <wallace07abreu@gmail.com>
2024-09-02 11:57:23 -0300

    post Done

--- src/controller/profile.js
+++ src/controller/profile.js
@@ -21,6 +21,42 @@ module.exports =
         console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
         res.render('../views/profile', {users,posts, id_user})
+    },
+
+    async updateProfile(req, res){
+
+        const id_user = req.params.id;
+        const dados = req.body;
+
+        if (req.file) {
+            // Recebendo a antiga foto do aluno
+            const users= await user.findOne({
+                raw: true,
+                attributes: ['profilePhoto'],
+                where: {idUser: id_user}
+            })
+            // Excluindo a foto da pasta
+            if (users.profilePhoto != 'usuario.png') fs.unlink(`public/img/${users.profilePhoto}`, ( err => { if(err) console.log(err); } ));
+            // Update da nova foto no DB
+            await aluno.update(
+            {profilePhoto: req.file.filename},
+            {where: { idUser: id_user  }}
+            );
+            }
+
+
+        await user.update({
+            name: dados.modalName,
+            password: dados.modalPassword,
+            birthDate: dados.modalBirth,
+            description: dados.modalBio,
+            email: dados.modalEmail,
+        },
+    
+    {
+        where :{idUser : id_user} 
+    });
+    res.redirect('/profilePag/:id');
     }
 
 }

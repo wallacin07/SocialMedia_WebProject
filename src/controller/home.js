57a893b95af6d1b2d5d3a3c4f7e834b8a945e685
CrCardd <cris.rc377@gmail.com>
2024-09-06 11:08:10 -0300

    preparações para merge

--- src/controller/home.js
+++ src/controller/home.js
@@ -11,11 +11,6 @@ module.exports = {
         const dados = req.body
         
         const id_user = req.params.id_user;
-        let followed = '0'
-        let followedIds = '0'
-        
-        let followedPosts = '0'
-        let nonFollowedPosts = '0'
 
 
 
@@ -24,10 +19,6 @@ module.exports = {
             attributes: ['idUser','name','password', 'birthDate','description','email','profilePhoto','admin','active']
         });
 
-        const comments = await comment.findAll({
-            raw: true,
-            attributes: ['idComment','description','commentDate', 'idPost','idUser']
-        });
 
 
 
@@ -59,18 +50,8 @@ module.exports = {
 
 
 
-        //=============================================
-       
-        //=============================================
 
 
-
-
-
-        const reactions = await reaction.findAll({
-            raw: true,
-            attributes: ['idReaction','description']
-        })
 
 
         res.render('../views/home', {users,comments:'0',reactions:'0', id_user, followedPosts});

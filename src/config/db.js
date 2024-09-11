e9f3208a731e752b0256c84fb18e740d1694badf
sasa2754 <sasamor5@gmail.com>
2024-09-11 14:09:29 -0300

    inicio de correções

--- src/config/db.js
+++ src/config/db.js
@@ -10,10 +10,10 @@ const database = new sequelize (
     // '123321',            //PASSWORDD
     {
     host: 'localhost',
-    port: 1433, // Porta padrão sala perfeita
+    // port: 1433, // Porta padrão sala perfeita
     // port: 56604,
     // port: 58868,    //cr
-    // port: 54317, // Porta Sabrina sala fedida
+    port: 54317, // Porta Sabrina sala fedida
     // port: 3306, // Porta Sabrina casa
     dialect: 'mssql' 
     }

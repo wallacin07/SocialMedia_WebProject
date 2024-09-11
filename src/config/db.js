81732a463fe214e67ea38d9a77ca0c9cf94b62cf
sasa2754 <sasamor5@gmail.com>
2024-09-10 21:08:05 -0300

    Agr ta tudo responsavel

--- src/config/db.js
+++ src/config/db.js
@@ -3,16 +3,19 @@ const sequelize = require('sequelize');
 const database = new sequelize (
 
     'Kangoroo',         //DATABASE
-    'Admin',            //USER
-    'admin',            //PASSWORDD
+    // 'Admin',            //USER
+    // 'admin',            //PASSWORDD
+
+    'root',            //USER
+    '123321',            //PASSWORDD
     {
     host: 'localhost',
-    port: 1433, // Porta padrão sala perfeita
+    // port: 1433, // Porta padrão sala perfeita
     // port: 56604,
     // port: 58868,    //cr
     // port: 54317, // Porta Sabrina sala fedida
-    // port: 3306, // Porta Sabrina casa
-    dialect: 'mssql' 
+    port: 3306, // Porta Sabrina casa
+    dialect: 'mysql' 
     }
 );
 
